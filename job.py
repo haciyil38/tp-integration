ce4c8c22678b97ba072bb404f8a8e93fec597144
Haci Yilmazer <lla.yilmazer.haci@gmail.com>
2026-09-03 15:44:09 +0200

    utilisation secret python

--- job.py
+++ job.py
@@ -1,3 +1,12 @@
+import os
+
 a = 2
 
 print("coucou", a)
+
+secret = os.environ.get("SECRET_API_TOKEN")
+
+if secret:
+    print("Le secret est bien récupéré")
+else:
+    print("Le secret n'est pas disponible")
